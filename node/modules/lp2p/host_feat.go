1d32583c704a1973457d66accec40059ceb3bfe1
alien <weitaibin@foxmail.com>
2021-03-22 18:27:15 +0800

    p2p test

---
+++ node/modules/lp2p/host_feat.go
@@ -0,0 +1,44 @@
+package lp2p
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/libp2p/go-libp2p"
+	"go.uber.org/fx"
+
+	"github.com/filecoin-project/lotus/build"
+)
+
+func Host1(lc fx.Lifecycle, params P2PHostIn) (RawHost, error) {
+	ctx := context.TODO()
+
+	pkey := params.Peerstore.PrivKey(params.ID)
+	if pkey == nil {
+		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())
+	}
+
+	opts := []libp2p.Option{
+		libp2p.Identity(pkey),
+		libp2p.Peerstore(params.Peerstore),
+		libp2p.NoListenAddrs,
+		libp2p.Ping(true),
+		libp2p.UserAgent("lotus-" + build.UserVersion()),
+	}
+	for _, o := range params.Opts {
+		opts = append(opts, o...)
+	}
+
+	h, err := libp2p.New(ctx, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	lc.Append(fx.Hook{
+		OnStop: func(ctx context.Context) error {
+			return h.Close()
+		},
+	})
+
+	return h, nil
+}
